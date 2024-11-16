715b642ea38cd586bba66b014a3279ecfc7f5636
Zhao Ren Mao <990203955@qq.com>
2024-11-16 08:38:05 +0800

    更新 roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -29,6 +29,7 @@ git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-
 git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
+git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
